861ea96a0f6edfab8c94d1a5c7e6e04d6581ab95
my-reacter <yaoqqiulin@gmail.com>
2019-06-09 09:40:07 +0800

    12

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -1,6 +1,18 @@
 const App = props=>{
     return (<div>
-        嵌套路由的首页哦！点击上面的List试试
+        pages
+            |
+            content
+            |   |
+            |    example(components----models----services) + index.js/less
+            |   |
+            |   _layout.js+index.js+list.js
+            |
+            example
+            |    example(components----models----services) + index.js/less
+            |   |
+            |   index.js/less
+            404.js/less+index.css/js
     </div>)
 }
 export default App;
